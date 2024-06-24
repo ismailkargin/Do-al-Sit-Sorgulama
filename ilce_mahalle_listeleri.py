26df99d179fc96508700a7cebf4b5e822cd46cb9
İsmail KARGIN <smail>
2024-06-24 21:11:58 +0300

    İlçe-mahalle listesi tamamlandı

---
+++ ilce_mahalle_listeleri.py
@@ -0,0 +1,11 @@
+
+ilceler = ["Datça", "Marmaris"]
+
+
+mahalleler = {
+
+    "Datça": ["Cumalı", "Datça", "Emecik", "Hızırşah", "İskele", "Karaköy", "Kızlan", "Mesudiye", "Reşadiye", "Sındı", "Yaka", "Yazı"],
+
+    "Marmaris": ["Ada", "Armutalan", "Bayır", "Beldibi", "Bozburun", "Çamlı", "Çetibeli", "Hisarönü", "İçmeler", "Karaca", "Kemeraltı",
+                 "Orhaniye", "Osmaniye", "Selimiye", "Söğüt", "Taşlıca", "Tepe", "Turgut", "Turunç"]
+}
